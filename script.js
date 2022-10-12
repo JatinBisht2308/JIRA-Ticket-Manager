ab5b292c568280bb58b1d29d9cfa98396cdc41ac
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-10-12 06:39:11 +0530

    :fire: created the modal and styled it

---
+++ script.js
@@ -0,0 +1,7 @@
+// first logic is for opening the modal while clicking on the plus button
+let plusBtn = document.querySelector(".add-btn");
+plusBtn.addEventListener("click", (e)=>{
+//   display the modal 
+
+// generate ticket
+})
