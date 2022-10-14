a0fcc0136797bda477282343287300c3ad85c634
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-10-14 20:38:23 +0530

    :fire: done with the modal functionality now adding the tickets in the main container

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // first logic is for opening the modal while clicking on the plus button
 let plusBtn = document.querySelector(".add-btn");
+// this flag will be true if modal open and false if not.
+let addFlag = false;
+// this will make the modal open or close
+let modalCont = document.querySelector(".modal");
 plusBtn.addEventListener("click", (e)=>{
 //   display the modal 
-
+     addFlag = !addFlag;
+     if(addFlag){
+        modalCont.style.display = "flex";
+     }
+     else{
+        modalCont.style.display = "none";
+     }
 // generate ticket
-})
+});
